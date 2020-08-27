968122342e4c6cec30af16dda725de9fb68b6230
LenovaPC <LenovaPC@DESKTOP-9FDNQSH>
2020-08-27 10:45:23 +0530

    commit

---
+++ Hashset/src/Hsahsetdemo.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Hsahsetdemo {
+
+	public static void main(String[] args) {
+		HashSet h=new HashSet();
+		h.add("B");
+		h.add("H");
+		h.add("C");
+		h.add("Z");
+		h.add("null");
+		h.add("10");
+		System.out.println(h.add("Z"));
+		System.out.println(h);
+
+	}
+
+}
